a6d3e86b5b6dde0dce1817c1fd6407482278fb57
Muhammad Ishtobir Fuady <tobyfuady.17201@gmail.com>
2024-02-15 20:30:21 +0700

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,53 +1,60 @@
-function calculate(s1, s2, value) {
+function calculate() {
+    let s1 = document.getElementById("temperature-unit-1").value;
+    let s2 = document.getElementById("temperature-unit-2").value;
+    let value = parseInt(document.getElementById("temperature-value").value);
+
+    let result = 0;
     if(s1==s2) {
-        return value;
+        result = value;
     }
 
     else if(s1=="celsius" && s2=="fahrenheit") {
-        return (value * 9 / 5) + 32;
+        result = (value * 9 / 5) + 32;
     }
 
     else if(s1=="celsius" && s2=="kelvin") {
-        return (value + 273);
+        result = (value + 273);
     }
 
     else if(s1=="celsius" && s2=="reamur") {
-        return (value * 4 / 5);
+        result = (value * 4 / 5);
     }
 
     else if(s1=="fahrenheit" && s2=="celsius") {
-        return (value - 32) * 5 / 9;
+        result = (value - 32) * 5 / 9;
     }
 
     else if(s1=="fahrenheit" && s2=="reamur") {
-        return (value - 32) * 4 / 9;
+        result = (value - 32) * 4 / 9;
     }
 
     else if(s1=="fahrenheit" && s2=="kelvin") {
-        return (value - 32) * 5 / 9 + 273;
+        result = (value - 32) * 5 / 9 + 273;
     }
 
     else if(s1=="kelvin" && s2=="celsius") {
-        return (value - 273);
+        result = (value - 273);
     }
 
     else if(s1=="kelvin" && s2=="fahrenheit") {
-        return (value - 273) * 9 / 5 + 32;
+        result = (value - 273) * 9 / 5 + 32;
     }
 
     else if(s1=="kelvin" && s2=="reamur") {
-        return (value - 273) * 4 / 5;
+        result = (value - 273) * 4 / 5;
     }
 
     else if(s1=="reamur" && s2=="celsius") {
-        return (value * 5 / 4);
+        result = (value * 5 / 4);
     }
 
     else if(s1=="reamur" && s2=="fahrenheit") {
-        return (value * 9 / 4) + 32;
+        result = (value * 9 / 4) + 32;
     }
 
     else if(s1=="reamur" && s2=="kelvin") {
-        return (value * 5 / 4) + 273;
+        result = (value * 5 / 4) + 273;
     }
+
+    document.getElementById("result").innerText = result;
 }
